25abf53cb75302a2e4eb8202183e864fabba7c33
Niko Känsäkoski <niko.kansakoski@student.oulu.fi>
2021-10-28 13:38:08 +0300

    negation prefix, config extra, csv save not working

--- 5_part_of_speech_first_last.py
+++ 5_part_of_speech_first_last.py
@@ -2,6 +2,9 @@ import nlp_config as config
 import nlp_helpers as helpers
 import preprocessing
 import csv
+from nltk import pos_tag
+import matplotlib.pyplot as plt
+
 
 # constants
 results_path = 'results/5/'
@@ -13,6 +16,59 @@ results_path = 'results/5/'
 # across all lines of the poem in both ebooks. How this variation takes place across different chapters.
 # Use corresponding illustrations to justify your answers. Comment on the phonetic compatibility of poem in each ebook.
 
+def tag_words(book):
+
+    tags = []
+
+    for title, chapter in book.items():
+        for line in chapter:     
+            print(line)    
+            try:   
+                tag = pos_tag(line)
+
+                tags.append((tag[0][1], tag[1][1]))
+            except IndexError as e:
+                print(e)
+    
+    return tags
+
+
+def save_to_CSV(data, path):
+    with open(path + '.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for pair in data:
+            csv_writer.writerow([pair[0], pair[1]])
+
+def plot(path: str, structure: list, title: str):
+    unique = list(set(structure))
+
+    freq = []
+
+    for u in unique:
+        c = structure.count(u)
+        freq.append(c)
+
+    zipped = zip(unique, freq)
+    
+    s = sorted(zipped, key=lambda x: x[1], reverse=True)
+    
+    for _ in s:
+        plt.bar(_[0], _[1], color="blue")
+    mng = plt.get_current_fig_manager()
+    #mng.resize(*mng.window.maxsize()) 
+    """
+    This SHOULD resize the image to maximum size, but boy does it not do that,
+    the image will be *shown* in maximum size, but not saved ¯\_(ツ)_/¯ 
+    """
+    mng.full_screen_toggle()
+    plt.xlabel("Part of speech tag")
+    plt.ylabel("Frequency")
+    plt.title(title)
+    
+    if config.action == "save":
+        plt.savefig(path)
+    else:
+        plt.show()
 
 def task5():
 
@@ -24,6 +80,15 @@ def task5():
 
         chapters = preprocessing.preprocess(raw_book)
         # TODO
+        first_last = preprocessing.only_first_and_last_words(chapters)
+
+        tags = tag_words(first_last)
+
+        save_to_CSV(tags, results_path + book_ref + "_pos_f_l")
+
+        title = book_ref + " first and last word part of speech tag"
+
+        plot(results_path + book_ref + "_f_l_pos.png", tags, title)
 
 
 if __name__ == "__main__":
--- nlp_config.py
+++ nlp_config.py
@@ -1,10 +1,10 @@
 # sources
 books = dict(
-    keats_lamia=dict(
-        title='LAMIA',
-        author='Keats',
-        path='keats.txt',
-    ),
+    #keats_lamia=dict(
+    #    title='LAMIA',
+    #    author='Keats',
+    #    path='keats.txt',
+    #),
     blake_songs_of_innocence=dict(
         title='SONGS OF INNOCENCE',
         author='Blake',
@@ -13,9 +13,13 @@ books = dict(
 )
 
 # actions
-action = 'save'  # 'save' to save plots into files, any other value to visualize results
+action = 'sav'  # 'save' to save plots into files, any other value to visualize results
 plot_bar_color = 'blue'
 
 # preprocessing data
 special_characters = [",", ".", "/", "\"", ";", "-", "_", "!", "?", "(", ")", "--", ".\"", "!--", ",\"", ".--", "'", ":", "*", '""', '"', "``", "''", "'d", "'s", "l.", "Cf", "Keats", "Lawrence", 'Cf', '’', '—', '‘']
 negations = ["no", "none", "never", "isn't", "ain't", "doesn't", "wont", "nothing", "don't", "nowhere", "not", "hasn't", "hadn't", "couldn't", "shouldn't", "wouldn't", "didn't", "aren't", "mightn't", "wasn't", "weren't", "mustn't", "needn't", "shan't", "nor", "needn't", "nobody", "nothing", "neither", "none", "hardly", "scarcely", "barely", "cannot"]
+negation_prefix = ["im", "non", "un", "in", "de", "dis", "a", "anti", "il", "ir"]
+
+#Select corpus
+corpus = "bown" # Can be slow, default to nltk.corpus.words if not specified to be brown
